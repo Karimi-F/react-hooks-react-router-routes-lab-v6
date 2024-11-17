b3b4f130c4e58e5b909f787f6d99deefeac8d55d
Karimi-F <fridah37karimi@gmail.com>
2024-11-17 22:06:36 +0300

    Submits

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import React from "react";
+
+const router = createMemoryRouter(routes,{
+    initialEntries:["/"],
+    initialIndex:0,
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(
+        <RouterProvider router={router}/>
+);
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,14 +1,35 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+const{id} = useParams();
+const [movie,setMovie] = useState(null);
+
+useEffect(() =>{
+  fetch(`http://localhost:4000/movies/${id}`)
+  .then((response) => response.json())
+  .then((data) => setMovie(data))
+  .catch((error) => console.error("Error fetching movie:",error));
+},[id]);
+
+if(!movie){
+  return <p>Loading...</p>
+}
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
+        <NavBar />
+      
+        <h1>{movie.title}</h1>
+        <p>Time: {movie.time}</p>
+        <div>
+          <h3>Genres: </h3>
+          {movie.genres.map((genre, index) => (
+            <span key={index} >{genre}</span>
+          ))}
+        </div>
+   
     </>
   );
 };
